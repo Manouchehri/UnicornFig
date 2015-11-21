d68fe0c8555f8be91b7d376d17926f5ddf2618ee
Zack Mullaly <redwire@riseup.net>
2015-11-21 13:48:36 -0500

    Wrote a badass build script to format go files, run unit tests, and try to build the interpreter

---
+++ build.sh
@@ -0,0 +1,28 @@
+#! /usr/bin/env sh
+
+echo "Formatting go files"
+go fmt src/*.go
+go fmt src/interpreter/*.go
+go fmt src/stdlib/*.go
+
+echo ""
+echo "Running unit tests."
+cd src/interpreter
+echo "  * Intepreter"
+go test
+cd ../stdlib
+echo "  * Standard Library"
+go test
+cd ..
+echo "  * Unicorn"
+go test
+cd ..
+
+echo ""
+echo "Building Unicorn"
+go build src/unicorn.go
+if [ $? -eq 0 ]; then
+    echo "[SUCCESS] - Built Unicorn"
+else
+    echo "[FAILURE] - Could not build Unicorn"
+fi
