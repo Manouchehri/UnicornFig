2cb7e46dc069d75713405b01ba13f79f81f68277
Zack Mullaly <redwire@riseup.net>
2015-11-20 19:13:55 -0500

    Added some handy math and string functions

--- src/stdlib/math.go
+++ src/stdlib/math.go
@@ -15,3 +15,40 @@ func SLIB_Multiply(env uni.Environment, arguments ...interface{}) (error, uni.Va
   }
   return nil, uni.NewInteger(product), env
 }
+
+func SLIB_Divide(env uni.Environment, arguments ...interface{}) (error, uni.Value, uni.Environment){
+  if len(arguments) < 2 {
+    return errors.New("Divide function expects two or more arguments."), uni.Value{}, env
+  }
+  result := arguments[0].(int64)
+  for i := 1; i < len(arguments); i++ {
+    next := arguments[i].(int64)
+    if next == 0 {
+      return errors.New("Cannot divide by zero."), uni.Value{}, env
+    }
+    result /= next
+  }
+  return nil, uni.NewInteger(result), env
+}
+
+func SLIB_Add(env uni.Environment, arguments ...interface{}) (error, uni.Value, uni.Environment){
+  if len(arguments) < 2 {
+    return errors.New("Add function expects two or more arguments."), uni.Value{}, env
+  }
+  sum := arguments[0].(int64)
+  for i := 1; i < len(arguments); i++ {
+    sum += arguments[i].(int64)
+  }
+  return nil, uni.NewInteger(sum), env
+}
+
+func SLIB_Subtract(env uni.Environment, arguments ...interface{}) (error, uni.Value, uni.Environment){
+  if len(arguments) < 2 {
+    return errors.New("Subtract function expects two or more arguments."), uni.Value{}, env
+  }
+  result := arguments[0].(int64)
+  for i := 1; i < len(arguments); i++ {
+    result -= arguments[i].(int64)
+  }
+  return nil, uni.NewInteger(result), env
+}
--- src/stdlib/stdlib.go
+++ src/stdlib/stdlib.go
@@ -5,6 +5,15 @@ import (
 )
 
 var StandardLibrary uni.Environment = uni.Environment {
-  "mult": uni.NewCallableFunction("mult", []string{"a", "b"}, SLIB_Multiply),
+  "mul": uni.NewCallableFunction("mul", []string{"a", "b"}, SLIB_Multiply),
+  "div": uni.NewCallableFunction("div", []string{"a", "b"}, SLIB_Divide),
+  "add": uni.NewCallableFunction("add", []string{"a", "b"}, SLIB_Add),
+  "sub": uni.NewCallableFunction("sub", []string{"a", "b"}, SLIB_Subtract),
+  "concat": uni.NewCallableFunction("concat", []string{"s1", "s2"}, SLIB_Concatenate),
+  "substr": uni.NewCallableFunction("substr", []string{"str", "start", "end"}, SLIB_Substring),
+  "index": uni.NewCallableFunction("index", []string{"s1", "s2"}, SLIB_Index),
+  "length": uni.NewCallableFunction("length", []string{"str"}, SLIB_Length),
+  "upcase": uni.NewCallableFunction("upcase", []string{"str"}, SLIB_Upcase),
+  "downcase": uni.NewCallableFunction("downcase", []string{"str"}, SLIB_Downcase),
   "print": uni.NewCallableFunction("print", []string{"msg"}, SLIB_Print),
 }
